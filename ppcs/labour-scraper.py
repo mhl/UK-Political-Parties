42e06b5bb1edc5aa75ca543338515c8fea0036b9
Mark Longair <mhl@pobox.com>
2014-11-30 17:04:23 +0000

    Update to Labour PPC scraper to use their new page
    This scrapes people from
    http://www.labour.org.uk/people/filter/c/candidate instead, which is
    supposedly more up-to-date, although it finds fewer people.

--- ppcs/labour-scraper.py
+++ ppcs/labour-scraper.py
@@ -1,79 +1,79 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+import json
 import re
 import requests
+import sys
 from urlparse import urljoin
 
 from common import get_empty_json_directory, write_ppc_json
 
-base_url = 'http://www2.labour.org.uk'
-all_page = '/candidates'
-
-r = requests.get(urljoin(base_url, all_page))
-main_soup = BeautifulSoup(r.text)
+base_url = 'http://www.labour.org.uk/people/filter/c/candidate'
 
 json_directory = get_empty_json_directory('labour')
 
-table = main_soup.find('table')
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text)
 
-matchers = {
-    'email': re.compile(r'(?ms)Email:.*?<a.*?href="mailto:(.*?)"'),
-    'twitter_username': re.compile(r'(?ms)Twitter:.*?<a.*?href=".*?twitter.com/(.*?)"'),
-    'phone': re.compile(r'(?ms)Tel:.*?([\d\s\(\)]+)'),
-    'facebook_url': re.compile(r'(?ms)Facebook:.*?<a.*?href="(.*?)"'),
+main_content_div = soup.find('div', id='main-content')
+
+key_mapping = {
+    'twitter': 'twitter_username',
+    'telephone': 'phone',
+    'facebook': 'facebook_url',
+    'web': 'homepage',
+    'address': 'address',
+    'email': 'email',
 }
 
-def get_person(full_name, constituency, relative_url):
-    full_url = urljoin(base_url, relative_url)
-    full_url = re.sub(r'\?.*', '', full_url)
-    print "got URL:", full_url
-    data = {
-        'path': relative_url,
-        'full_url': full_url,
-    }
-    r = requests.get(full_url)
-    if r.status_code != 200:
-        print "Warning: bad HTTP status returned for", full_url
-        return data
-    page_text = r.text
-    person_soup = BeautifulSoup(page_text)
-    title = person_soup.find('h2')
-    if not title:
-        print "Warning: couldn't find a title in", full_url
-        _, full_name = [x.strip() for x in title.text.split(',')]
-        # Remove any NO-BREAK-SPACE from the name:
-        data['name'] = re.sub(r'\xA0', ' ', full_name)
-    # Unfortunately, these pages are malformed enough that when parsed
-    # by BeautifulSoup, the <p/>s that contain some of the contact
-    # details disappear. So (sigh) just extract them with regular expressions.
-    page_text = page_text.replace("\r", '').replace("\n", '')
-    for key, regex in matchers.items():
-        m = regex.search(page_text)
-        if m:
-            data[key] = m.group(1).strip()
-    return data
+def get_person(full_name, constituency, person_url):
+    result = {}
+    print "Processing", person_url
+    r = requests.get(person_url)
+    person_soup = BeautifulSoup(r.text)
+    address_div = person_soup.find('address', {'class': 'person-address'})
+    if address_div is None:
+        print person_soup.prettify()
+    for address_link in address_div.find_all('a', {'class': 'person-address-link'}):
+        classes = [c for c in address_link['class'] if c != 'person-address-link']
+        if len(classes) != 1:
+            raise Exception, "Found unexpected number of classes remaining:", classes
+        info_key = key_mapping[classes[0]]
+        info_value = address_link.text.strip()
+        info_url = address_link['href']
+        if info_key == 'twitter_username':
+            m = re.search(r'twitter.com/(\S+)', info_url)
+            result[info_key] = m.group(1)
+        elif info_key == 'phone':
+            result[info_key] = info_value
+        elif info_key == 'facebook_url':
+            result[info_key] = info_url
+        elif info_key == 'email':
+            m = re.search(r'mailto:(.*)', info_url)
+            result[info_key] = m.group(1)
+        elif info_key == 'homepage':
+            result[info_key] = info_url
+        elif info_key == 'address':
+            result[info_key] = info_value
+    return result
 
-for row in table.find_all('tr'):
-    cells = row.find_all('td')
-    constituency, region, candidate = [
-        c.text.strip() for c in cells
-    ]
-    if not candidate:
-        continue
-    if constituency == 'Constituency':
-        # Then this is a header line, just skip it
-        continue
-    candidate_link = cells[2].find('a')
-    data = {
-        'name': candidate,
-        'region': region,
+for figure in main_content_div.find_all('figure'):
+    image = figure.find('img')
+    full_url = figure.parent['href']
+    name = figure.find('div', {'class': 'person-name'}).text.strip()
+    title_text = figure.find('div', {'class': 'person-title'}).text.strip()
+    constituency = re.sub(r'Candidate\s+for\s+(.*?)\s*$', '\\1', title_text)
+    result = {
+        'name': name,
         'constituency': constituency,
+        'full_url': full_url,
+        'image_headshot_url': image['src']
     }
-    if candidate_link:
-        data.update(get_person(
-            candidate_link,
-            constituency,
-            candidate_link['href']
-        ))
-    write_ppc_json(data, constituency, json_directory)
+    # Now get the person page:
+    result.update(get_person(
+        name,
+        constituency,
+        full_url,
+    ))
+    write_ppc_json(result, constituency, json_directory)
